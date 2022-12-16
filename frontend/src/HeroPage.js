079d13bc44b150bd89d5bc6a629bcb5592cc311e
pgscastro <33170789+pgscastro@users.noreply.github.com>
2022-12-15 23:14:54 -0800

    heropage added

---
+++ frontend/src/HeroPage.js
@@ -0,0 +1,35 @@
+
+import  "./HeroPage.css";
+
+
+const Login = (props) => {
+     return (
+
+        <div className="background">
+            <div className="register-box">
+                <img className="logo" src={require('./logo.jpeg')} />
+                <h1>
+                    Register Here
+                </h1>
+                <form>
+                    <p>e-mail</p>
+                    <input type="e-mail" name="" placeholder="e-mail"/>
+                    <p>Username</p>
+                    <input type="text" name="" placeholder="Username"/>
+                    <p>Password</p>
+                    <input type="password" name="" placeholder="Password"/>
+                    <input type="submiit" name ="" value ="Register"/>
+                    <a href ="#">Already Registered? Login here.</a> 
+                    
+
+                </form>
+            </div>
+
+        </div>
+);
+       
+        
+
+}
+
+export default Login;
